ee5ad51e8e7dd61b2f778331a2b3f2775b855a1d
developersunesis <developersunesis@gmail.com>
2020-09-01 05:40:02 +0100

    Added barcode page

--- src/schoolar/App/SchoolarRoutes.js
+++ src/schoolar/App/SchoolarRoutes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import QRCode from 'react-qr-code'
 
 function SchoolarRoutes({ children }) {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/'>
-          <div>Barcode Activity testing</div>
+          <div>
+            <QRCode value='http://192.168.8.1:5000' />
+          </div>
         </Route>
         {children}
       </Switch>
